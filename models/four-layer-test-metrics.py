2bba420c3e5a4947f364060977db80b33cd17a18
ztime <jonas@lechuck.se>
2019-10-22 14:22:52 +0200

    Testing new loss function

--- models/four-layer-test-metrics.py
+++ models/four-layer-test-metrics.py
@@ -7,33 +7,33 @@ from keras import backend as K
 def get_description():
     desc = ["4 layers:"]
     desc.append("convlstm - 128 filters")
-    desc.append("convlstm - 64 filters")
+    desc.append("convlstm - 128 filters")
     desc.append("convlstm - 64 filters")
     desc.append("convlstm - 1 filters")
-    desc.append("Mean squared error ada delta")
-    desc.append("Testing differnt loss functions as metrics to see if it gives any insights")
+    desc.append("Using Kullback Leibler divergence as loss function instead")
+    desc.append("Also changed the second layer to 128 filters, see if we get overload")
     return '\n'.join(desc)
 
 def get_model(sequence_length, img_width, img_height):
     model = _build_network(sequence_length, img_width, img_height)
     model.compile(
-            loss='mean_squared_error',
+            loss='kullback_leibler_divergence',
             optimizer='adadelta',
             metrics=[
                 'accuracy',
                 'mean_squared_error',
-                'mean_absolute_error',
-                'mean_absolute_percentage_error',
-                'mean_squared_logarithmic_error',
-                'squared_hinge',
-                'hinge',
-                'logcosh',
-                # 'huber_loss',
-                'sparse_categorical_crossentropy',
-                'binary_crossentropy',
-                'kullback_leibler_divergence',
-                'poisson',
-                'cosine_proximity',
+                # 'mean_absolute_error',
+                # 'mean_absolute_percentage_error',
+                # 'mean_squared_logarithmic_error',
+                # 'squared_hinge',
+                # 'hinge',
+                # 'logcosh',
+                # # 'huber_loss',
+                # 'sparse_categorical_crossentropy',
+                # 'binary_crossentropy',
+                # 'kullback_leibler_divergence',
+                # 'poisson',
+                # 'cosine_proximity',
                 ]
             )
     return model
@@ -52,7 +52,7 @@ def _build_network(sequence_length, img_width, img_height):
     model.add(BatchNormalization())
     model.add(
             ConvLSTM2D(
-                filters=64,
+                filters=128,
                 kernel_size=(3,3),
                 padding='same',
                 return_sequences=True,
