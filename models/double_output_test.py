651a172dda087ba9f1a32f485f49f9afd9b535d9
ztime <jonas@lechuck.se>
2019-10-23 16:07:30 +0200

    Trying to understand the output

--- models/double_output_test.py
+++ models/double_output_test.py
@@ -33,13 +33,13 @@ def _build_network(sequence_length, img_width, img_height):
     input_layer = Input(shape=(sequence_length, img_width, img_height, 1))
     encoder = ConvLSTM2D( filters=128, kernel_size=(3,3), padding='same', return_sequences=True)(input_layer)
     # Reconstruct decoder
-    decoder1 = RepeatVector(sequence_length)
-    decoder1 = ConvLSTM2D(filters=128, kernel_size=(3,3), padding='same', return_sequences=True)(decoder1)
+    # decoder1 = RepeatVector(sequence_length)(encoder)
+    decoder1 = ConvLSTM2D(filters=128, kernel_size=(3,3), padding='same', return_sequences=True)(encoder)
     decoder1 = TimeDistributed(Conv3D(filters=1, kernel_size=(3,3,3), padding='same', activation='sigmoid', data_format='channels_last'))(decoder1)
     # Predict decoder
-    decoder2 = RepeatVector(sequence_length - 1)
-    decoder2 = ConvLSTM2D(filters=128, kernel_size=(3,3), padding='same', return_sequences=True)(decoder2)
-    decoder2 = TimeDistributed(Conv3D(filters=1, kernel_size=(3,3,3), padding='same', activation='sigmoid', data_format='channels_last'))(decoder2)
+    # decoder2 = RepeatVector(sequence_length - 1)(encoder)
+    decoder2 = ConvLSTM2D(filters=128, kernel_size=(3,3), padding='same', return_sequences=False)(encoder)
+    decoder2 = Conv3D(filters=1, kernel_size=(3,3,3), padding='same', activation='sigmoid', data_format='channels_last')(decoder2)
     # together
     model = Model(inputs=input_layer, outputs=[decoder1, decoder2])
     return model
