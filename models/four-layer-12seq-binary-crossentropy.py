7571704f504d373f420157696180b97c33b0bc63
ztime <jonas@lechuck.se>
2019-10-28 14:23:37 +0100

    updated model

--- models/four-layer-12seq-binary-crossentropy.py
+++ models/four-layer-12seq-binary-crossentropy.py
@@ -3,10 +3,12 @@ from keras.models import Sequential, Model
 from keras.optimizers import Adam, RMSprop
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras import backend as K
+import numpy as np
 
 MODEL_OVERRIDES = {
         "sequence_length": 12,
         "batchsize": 1,
+        "data_prepare": True,
         }
 
 def get_description():
@@ -29,6 +31,24 @@ def get_model(sequence_length, img_width, img_height):
                 ]
             )
     return model
+
+def data_prepare(x_train, y_train):
+    """
+    For this model we have two outputs so we need two different
+    outputs for the loss function to use.
+
+    The first output is for decoder 1 which is the reverse of the input sequence
+    ALL of which has been seen in the training data
+    Second output is for decoder 2 which is the next frame in the sequence
+    which is not in the training data
+
+    x_train will remain the same for the data, so no need to touch that
+    """
+    total_sequences, sequence_length, img_width, img_height, img_channels = x_train.shape
+    y_train = np.reshape(y_train, (total_sequences, img_width, img_height, img_channels))
+
+    return x_train, y_train
+
 
 def _build_network(sequence_length, img_width, img_height):
     model = Sequential()
@@ -58,12 +78,20 @@ def _build_network(sequence_length, img_width, img_height):
                 )
         )
     model.add(
-            Conv3D(filters=1,
-                kernel_size=(3,3,3),
-                activation='sigmoid',
+            ConvLSTM2D(
+                filters=1,
+                kernel_size=(3,3),
                 padding='same',
-                data_format='channels_last',
+                return_sequences=False,
                 )
-            )
+        )
+    # model.add(
+            # Conv3D(filters=1,
+                # kernel_size=(3,3,1),
+                # activation='sigmoid',
+                # padding='same',
+                # data_format='channels_last',
+                # )
+            # )
     return model
 
